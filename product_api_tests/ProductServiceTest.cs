c3c069d332e6ef02c0c746cb95d357994767e8ec
Helena <helena@klarr.se>
2024-11-19 08:51:49 +0100

    test

--- product_api_tests/ProductServiceTest.cs
+++ product_api_tests/ProductServiceTest.cs
@@ -32,6 +32,7 @@ namespace product_api_tests
                 new Product { Id = "1", Name = "Product A", Color = "Red", Price = 10.0M, StockQuantity = 100 },
                 new Product { Id = "2", Name = "Product B", Color = "Blue", Price = 20.0M, StockQuantity = 200 },
                  new Product { Id = "3", Name = "Product C", Color = "Green", Price = 20.0M, StockQuantity = 300 }
+                   new Product { Id = "4", Name = "Product D", Color = "Green", Price = 20.0M, StockQuantity = 300 }
             };
             _mockRepository.Setup(repo => repo.GetAsync()).ReturnsAsync(products);
 
@@ -39,7 +40,7 @@ namespace product_api_tests
             var result = await _productService.GetAsync();
 
             // Assert: Verify the results
-            Assert.Equal(3, result.Count);                  // Check that two products are returned
+            Assert.Equal(4, result.Count);                  // Check that two products are returned
             Assert.Equal("Product A", result[0].Name);      // Verify the name of the first product
             _mockRepository.Verify(repo => repo.GetAsync(), Times.Once); // Ensure GetAsync was called once
         }
